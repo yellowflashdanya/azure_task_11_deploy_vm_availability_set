a12e8adcc315a314dd8f0c0914b7c31efb83380d
Danylo Melnychuk <danyakid2005@gmail.com>
2025-10-22 01:22:52 +0300

    Solution

--- task.ps1
+++ task.ps1
@@ -25,15 +25,24 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
+New-AzAvailabilitySet `
+    -Location $location `
+    -Name $availabilitySetName `
+    -ResourceGroupName $resourceGroupName `
+    -Sku aligned `
+    -PlatformFaultDomainCount 2 `
+    -PlatformUpdateDomainCount 2
+
 for (($zone = 1); ($zone -le 2); ($zone++) ) {
     New-AzVm `
     -ResourceGroupName $resourceGroupName `
     -Name "$vmName-$zone" `
     -Location $location `
-    -image $vmImage `
-    -size $vmSize `
+    -Image $vmImage `
+    -Size $vmSize `
     -SubnetName $subnetName `
     -VirtualNetworkName $virtualNetworkName `
     -SecurityGroupName $networkSecurityGroupName `
-    -SshKeyName $sshKeyName -Zone $zone
+    -AvailabilitySetName $availabilitySetName `
+    -SshKeyName $sshKeyName
 }
